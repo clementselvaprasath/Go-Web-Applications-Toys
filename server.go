153635d1eb5640cc8a15ac87ee847baa8a8f7bc3
Difan Chen <dfchen6@gmail.com>
2020-07-03 16:11:57 -0700

    Added gorilla/mux to route requests with preselected URL to handler

--- server.go
+++ server.go
@@ -2,16 +2,28 @@ package main
 
 import (
     "fmt"
-    "net/http"
+	"net/http"
+	
+	"github.com/gorilla/mux"
 )
 
 func main() {
-    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/books/{book_id}/pages/{page_id}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		bookID := vars["book_id"]
+		pageID := vars["page_id"]
+
+		fmt.Fprintf(w, "You are requested the book: %s on page %s\n", bookID, pageID)
+	})
+
+	r.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Welcome to my website!")
     })
 
     fs := http.FileServer(http.Dir("static/"))
     http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-    http.ListenAndServe(":80", nil)
+    http.ListenAndServe(":80", r)
 }
